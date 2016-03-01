60ca475ccb422b15cd8efd54eb87634099b7f296
Takatoshi Matsumoto <toqoz403@gmail.com>
2016-03-01 20:26:33 +0900

    add toqoz.hateblo.jp.{js,css}

---
+++ _src/javascripts/toqoz.hateblo.jp.js
@@ -0,0 +1,42 @@
+if (location.pathname === "/") {
+  boost(0);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  toArray(document.querySelectorAll('.date')).forEach(function(date) {
+    var time = date.querySelector('time');
+    var link = date.querySelector('a');
+    date.removeChild(link);
+    date.appendChild(time);
+  });
+});
+
+function boost() {
+  var selfScriptTag = document.currentScript;
+  var boostCount = 0;
+  _boost();
+  
+  function _boost() {
+    removeAll('iframe');
+    removeAll('script', function(node) {
+      return node !== selfScriptTag && node.getAttribute('src').indexOf('pushdog') === -1;
+    });
+    boostCount += 1;
+    console.log(boostCount);
+    if (boostCount < 20) {
+      setTimeout(_boost, boostCount * 100);
+    }
+  }
+}
+
+function removeAll(selector, test) {
+  toArray(document.querySelectorAll(selector)).forEach(function(node) {
+    if (!test || test(node)) {
+      node.parentNode.removeChild(node);
+    }
+  });
+}
+
+function toArray(ary) {
+  return Array.prototype.slice.apply(ary);
+}
